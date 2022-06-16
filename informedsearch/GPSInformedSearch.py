3f0fcfc140c5a83ae1ca508b7d85d7ccbc5d333f
Hugo da Silva da Silva <hugodasilvadasilva@gmail.com>
2022-06-16 07:55:51 -0300

    GPS using Branch and Bound tested, implemented and documented

--- informedsearch/GPSInformedSearch.py
+++ informedsearch/GPSInformedSearch.py
@@ -1,8 +1,20 @@
 from dis import dis
 import enum
+from tracemalloc import stop
 import unittest
 
+'''
+This block of code implements a GPS to find a route between 2 cities by using Branch and Bound algorithm,
+which uses only the cost (distance of each route from origin to current city) to define which route is
+more promessing. 
+'''
+
 class City:
+    '''
+    This class implements a City which is a concrete implementation of a node (for grapho)
+    or state (for search algorithm), where each City has a Id, latitude, longitude and 
+    a list of neighbour cities.
+    '''
 
     def __init__(self, id, lat: float, lon: float, neighbours: list):
         self.id = id
@@ -11,6 +23,12 @@ class City:
         self.neighbours = neighbours
     
 class Map(enum.Enum):
+    '''
+    The Map is a concrete implementation of a grapho. In this case, the Map
+    has a list of City.
+    GPSInformedSearch use this class to analyse routes and find path between origin
+    and destination.
+    '''
 
     Goiania = City("Goiania", -16.69, -49.25, ["Hidrolandia", "BelaVista"])
     Hidrolandia = City("Hidrolandia", -16.97, -49.22, ["Goiania", "ProfJamil", "BelaVista"])
@@ -35,32 +53,61 @@ class Map(enum.Enum):
         return []
     
     def calc_cartesian_distance(id_a: str, id_b: str):
+        '''
+        Calculate the cartesian distance between two cities by using their latitute
+        and longitude.
+        The cartesian distance is defined by calculating the square root of the sum
+        of each side at power of two.
+
+        ## Parameters
+        `- id_a: str` having the Id of CityA;
+        `- id_b: str` having the Id of CityB;
+        `- return: float` having the cartesian distance between `id_a` and `id_b`
+        '''
         cityA = Map[id_a].value
         cityB = Map[id_b].value
 
         dist = ((cityA.lat - cityB.lat) ** 2 + (cityA.lon - cityB.lon)**2)**(1/2)
         return dist
 
-class GPSInformedSearch:
+class GPSBranchAndBound:
+    '''
+    This class implements the Branch and Bound algorithm to find the distance between two cities.
+    For that you have to provide a `Map` as environment where each state is a `City`.
+    '''
 
     def __init__(self, map: Map):
+        '''
+        Initialize the class by getting a `Map` as parameter.
+
+        ## Parameters
+        `- map: Map` having the list of cities where each City has a list of neighbours.
+        '''
         self.__map = map
         self.__visited = []
         self.__routes = []
 
-    def search_route(self, ori: str, dst: str) -> list:
+    def search_route(self, ori: str, dst: str) -> tuple:
+        '''
+        Search a route between two cities (`ori` and `dst`) returning a tuple having a list 
+        of cities at position 0 and the total distance (total cost) by using this route.
+
+        ## Parameters
+        `ori: str` with origin city Id;
+        `dst: str` with destination city Id
+        '''
 
         # Add origin city to possible routes
-        self.__routes.append([ori])
+        self.__routes.append(([ori], 0))
 
         while self.__routes:
 
             # Get next route
             curr_route = self.__routes.pop(-1)
-            print(f"Analyse route {curr_route}")
+            print(f"Analysing route {curr_route}")
 
             # Get last city
-            curr_city = curr_route[-1]
+            curr_city = curr_route[0][-1]
 
             # Check if curr_city has been visited
             if curr_city in self.__visited:
@@ -85,22 +132,28 @@ class GPSInformedSearch:
                     print(f"{neighbour_id} will not be considered because has already been visited")
                     continue
 
-                dist = Map.calc_cartesian_distance(ori, neighbour_id)
-                print(f"Distance from {ori} to {neighbour_id} = {dist}")
+                dist = Map.calc_cartesian_distance(curr_city, neighbour_id)
+                print(f"Distance from {curr_city} to {neighbour_id} = {dist}")
+                
+                cost = curr_route[1] + dist
 
-                route = (neighbour_id, dist)
+                new_route = (curr_route[0] + [neighbour_id], cost)
+                print(f"New (route, dist) created = {new_route}")
 
-                new_routes.append(route)
+                # Include new_route into new_routes list
+                # new_routes list is decended sorted
+                index = 0
+                for r in self.__routes:
 
-            # Order new routes
-            sorted(new_routes, key=lambda r: r[1], reverse=True)
-            print(f"New routes sorted: {new_routes}")
-
-            # Add new routes to line of routes to be analysed
-            for new_route in new_routes:
-                self.__routes.append(curr_route + [new_route[0]])
-            
-            print(f"New routes added to line of routes to be analysed: {self.__routes}")
+                    # new_route will be included according to its distance 
+                    if new_route[1] > r[1]:
+                        self.__routes.insert(index, new_route)
+                        print(f"new_route {new_route} included at {index} position.")
+                        print(f"Waiting list of routes now is {self.__routes}")
+                        break
+                    index += 1
+                else:
+                    self.__routes.append(new_route)
 
         return None
 
@@ -125,8 +178,9 @@ class Test_Map_CalcCartesianDistance(unittest.TestCase):
 if __name__ == "__main__":
     #unittest.main()
 
-    gps = GPSInformedSearch(Map)
+    gps = GPSBranchAndBound(Map)
     origin = Map.Goiania.value.id
-    destination = Map.CaldasNovas.value.id
+    destination = Map.Cristianopolis.value.id
     r = gps.search_route(origin, destination)
     print(f"Route from {origin} to {destination} is {r}")
+    
